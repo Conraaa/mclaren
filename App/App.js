d0a36c3f26e5f5e1d09716c3259337651110495e
Conraaa <c.cemino1234@gmail.com>
2024-10-13 17:35:42 -0300

    elimine todo lo de funcion

--- App/App.js
+++ App/App.js
@@ -3,16 +3,7 @@ import './App.css';
 import Menu from './Imagenes/Menu.png';
 import Logo from './Imagenes/Logo.png';
 function App() {
-  return (
-    <div class='Barra'>
-      <img class="Menu" src={ Menu } alt="Botón Menú" />
-
-      <img class="Logo" src= { Logo } alt="logo de la pagina"/>
-      
-      <h4 class="Log">Log In</h4>  
-
-
-    </div>
+  
   );
 }
 
