977b7fe8b9351e8dfdef4eae7a09dc23c39f7b49
Conraaa <c.cemino1234@gmail.com>
2024-10-15 11:01:20 -0300

    Add files via upload

--- DesarrolloPy/mclaren/mclaren/database.py
+++ DesarrolloPy/mclaren/mclaren/database.py
@@ -1,13 +1,10 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
-# Define la URL de conexión a tu base de datos
 DATABASE_URL = "mssql+pyodbc://@localhost/mclaren?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
 
-# Crear un motor de base de datos
 engine = sa.create_engine(DATABASE_URL)
 
-# Crear una sesión
 Session = sessionmaker(bind=engine)
 
 def get_session():
@@ -19,16 +16,13 @@ from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Define la URL de la base de datos para SQL Server
 DATABASE_URL = "mssql+pyodbc://username:password@server_name/database_name?driver=ODBC+Driver+17+for+SQL+Server"
 
-# Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL, echo=True)
 
-# Crea una base de clase
 Base = declarative_base()
 
-# Define una tabla de ejemplo
+//table ejemplo
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
@@ -36,9 +30,7 @@ class User(Base):
     fullname = Column(String(50))
     nickname = Column(String(50))
 
-# Crear todas las tablas en la base de datos
 Base.metadata.create_all(bind=engine)
 
-# Crear una sesión para interactuar con la base de datos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 """
