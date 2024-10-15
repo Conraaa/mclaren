2f639f7742ec5b5b77238d527f328dc7e31640a5
Conraaa <c.cemino1234@gmail.com>
2024-10-15 01:20:12 -0300

    actualizo componente 1

--- App/Componente1/Componente1.js
+++ App/Componente1/Componente1.js
@@ -1,10 +1,1 @@
 import React from 'react'
-import './Componente1.css'
-import ReactPlayer from 'react-player'
-import Video from '../Video/this is mclaren.mp4'
-import PreVideo from '../Imagenes/Pre-Video.jpg'
-export default function Componente1() {
-  return (
-
-  )
-}
