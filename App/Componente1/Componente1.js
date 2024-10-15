38969a5b06f3477c9a883e844b6082f6d3dcf706
Conraaa <c.cemino1234@gmail.com>
2024-10-15 01:03:10 -0300

    Update Componente1.js

--- App/Componente1/Componente1.js
+++ App/Componente1/Componente1.js
@@ -5,12 +5,6 @@ import Video from '../Video/this is mclaren.mp4'
 import PreVideo from '../Imagenes/Pre-Video.jpg'
 export default function Componente1() {
   return (
-    <div class="video" >
-    <ReactPlayer url={ Video }
-    width={1342}
-    height={542}
-    playing
-    light={ PreVideo } />
-    </div>
+
   )
 }
