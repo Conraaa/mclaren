9c16ca0a3648170ff0ba675ba4e6092b5e1d2a88
MauLista02 <piquinclis@gmail.com>
2024-12-28 00:20:08 -0300

    lo ultimo que tengo

--- frontend/src/paginas/home.jsx
+++ frontend/src/paginas/home.jsx
@@ -1,82 +1,82 @@
 import React from 'react'
 import './home.css'
-
-import MC21 from '../Imagenes/MC21.png'
-import MC22 from '../Imagenes/MC22.png'
-import MC23 from '../Imagenes/MC23.png'
-import A21 from '../Imagenes/A21.jpg'
-import A22 from '../Imagenes/A22.jpg'
-import A23 from '../Imagenes/A23.jpg'
-import MC24 from '../Imagenes/MC24.png'
-import A24 from '../Imagenes/A24.png'
-import I21 from '../Imagenes/OscarLando.png'
-import I22 from '../Imagenes/OscarLando2.png'
-import I23 from '../Imagenes/Lando.png'
-import I24 from '../Imagenes/Oscar.png'
-import AA1 from '../Imagenes/2021.png'
-import AA2 from '../Imagenes/2022.png'
-import AA3 from '../Imagenes/2023.png'
-import AA4 from '../Imagenes/2024.png'
 import Bar from './Bar'
-
-
+import Video1 from '../Videos/Video4.mp4';
+import Video2 from'../Videos/Video5.mp4';
+import Video3 from '../Videos/Video2.mp4';
+import Video4 from '../Videos/Video6.mp4';
+import ReactPlayer from 'react-player';
 export default function home() {
-  const thumbs = document.querySelectorAll('.thumb li');
-  const items = document.querySelectorAll('.item');
-   let index = 0;
- 
- 
-   thumbs.forEach((thumb, ind) => {
-     thumb.addEventListener('click', (event) => {
-     
-         document.querySelector('.thumb .selected').classList.remove('selected')
-         thumb.classList.add('selected')
-         index = ind;
-         document.querySelector('.item.active').classList.remove('active')
-           items[index].classList.add('active')
- 
- 
-       })
-     });
- 
+
   return (
-    <div>  
+    <div class="Fondo">  
       <Bar/>
-<div class="carrusel">
-  <div class="item active">
-    <div class="img-box">
-      <img src={ MC21 } alt="Coche 2021" />
-      <img class="pilot" src={ I21 } alt="Pilotos 2021" />
-      <img class="anio" src={ AA1 } alt="Pilotos 2021" />
-    </div>
-  </div>
-  <div class="item">
-    <div class="img-box">
-      <img src={ MC22 } alt="Coche 2022" />
-      <img class="pilot" src={ I22 } alt="Pilotos 2021" />
-      <img class="anio" src={ AA2 } alt="Pilotos 2021" />
-    </div>
-  </div>
-  <div class="item">
-    <div class="img-box">
-    <img src={ MC23 } alt="Coche 2023" />
-    <img class="pilot" src={ I23 } alt="Pilotos 2021" />
-    <img class="anio" src={ AA3 } alt="Pilotos 2021" />
-    </div>
-  </div><div class="item">
-    <div class="img-box">
-    <img src={ MC24 } alt="Coche 2024" />
-    <img class="pilot" src={ I24 } alt="Pilotos 2021" />
-    <img class="anio" src={ AA4 } alt="Pilotos 2021" />
-    </div>
-  </div>
-    <ul class="thumb">
-    <li class="selected"><img src= { A21 } alt="Año 2022" /></li>
-    <li><img src= { A22 } alt="Año 2022" /></li>
-    <li><img src= { A23 } alt="Año 2022" /></li>
-    <li><img src= { A24 } alt="Año 2022" /></li>
-    </ul>
-</div>
+      <div className="CajaCarrusel">
+        <ul>
+            <li>
+            <ReactPlayer className="Video" url={ Video1 } 
+            playing
+            loop
+            muted
+            width="100%" 
+            height="100%" 
+            />
+                <div className="texto">
+                <h2>HOLA PUTOS</h2>
+                <p>PUTOS HOLA SADSADSADSADSAD</p>
+
+                </div>
+
+            </li>
+            <li>
+            <ReactPlayer className="Video" url={ Video2 } 
+            playing
+            loop
+            muted
+            width="100%" 
+            height="100%" 
+            />
+                <div className="texto">
+                    <h2>HOLA PUTOS</h2>
+                    <p>PUTOS HOLA SADSADSADSADSAD</p>
+                </div>
+
+            </li>
+            <li>
+            <ReactPlayer className="Video" url={ Video3 } 
+            playing
+            loop
+            muted
+            width="100%" 
+            height="100%" 
+            />
+                <div className="texto">
+                <h2>HOLA PUTOS</h2>
+                <p>PUTOS HOLA SADSADSADSADSAD</p>
+
+                </div>
+
+            </li>
+            <li>
+            <ReactPlayer className="Video" url={ Video4 } 
+            playing
+            loop
+            muted
+            width="100%" 
+            height="100%" 
+            />
+                <div className="texto">
+                <h2>HOLA PUTOS</h2>
+                <p>PUTOS HOLA SADSADSADSADSAD</p>
+
+                </div>
+
+            </li>
+
+
+        </ul>
+
+      </div>
     </div>
 
   )
--- frontend/src/paginas/sponsor.jsx
+++ frontend/src/paginas/sponsor.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './sponsor.css'
 import Bar from './Bar'
+import Video1 from '../Videos/mclarenf1.mp4'
+import ReactPlayer from 'react-player'
 export default function sponsor() {
   return (
     <div>  
       <Bar/>
+      <ReactPlayer url={ Video1 } 
+            playing
+            loop
+            muted
+            />
 </div>
   )
 }
