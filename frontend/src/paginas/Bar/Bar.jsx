5a8f9801474604fe58645f1d6285ec6cb386d7db
MauLista02 <piquinclis@gmail.com>
2025-01-27 19:05:25 -0300

    intro front

--- frontend/src/paginas/Bar/Bar.jsx
+++ frontend/src/paginas/Bar/Bar.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './Bar.css';
+import './intro/Intro.css';
 import Logo from '../Imagenes/Logo.png';
 import Logocentral from '../Imagenes/Logo_Central.png';
 import { Link } from 'react-router-dom';
+import Intro from './intro/Intro';
 
 export default function Bar() {
   const [userName, setUserName] = useState('');
   const [userDepartment, setUserDepartment] = useState('');
   const [userLegajo, setUserLegajo] = useState('');
+  const [introFinished, setIntroFinished] = useState(false); // Controla si el video de introducción terminó
 
   useEffect(() => {
     // Obtener los datos del usuario desde localStorage
@@ -24,89 +27,95 @@ export default function Bar() {
   }, []);
 
   return (
-    <div className="bar">
-      {/* Menú principal a la izquierda */}
-      <div className="logoMenú">
-        <nav>
-          <ul className="menuHorizontal">
-            <li>
-              <Link>
-                <img className="Logo" src={Logo} alt="logo de la pagina" />
-              </Link>
-              <ul className="menuVertical">
-                <li>
-                  <Link to="/">
-                    <button><b>Home</b></button>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/empleados">
-                    <button><b>Empleados</b></button>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/carreras">
-                    <button><b>Carreras</b></button>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/piezas">
-                    <button><b>Piezas</b></button>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/pistas">
-                    <button><b>Pistas</b></button>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/Estrategia">
-                    <button><b>Estrategia</b></button>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/Departamentos">
-                    <button><b>Departamento</b></button>
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <>
+      {/* Clip de introducción */}
+      {!introFinished && <Intro onFinish={() => setIntroFinished(true)} />}
 
-      {/* Logo central */}
-      <Link to="/">
-        <img className="Logo_central" src={Logocentral} alt="Logo de en medio" />
-      </Link>
-
-      {/* Menú de usuario a la derecha */}
-      <div className="userMenu">
-        {userName ? (
-          <>
-            <div className="userInfo">
-              <p className="user">{`${userName} - ${userDepartment}`}</p>
-            </div>
-            <ul className="menuVerticalUser">
-              <li><b>{userName}</b></li>
-              <li><b>{userDepartment}</b></li>
-              <li><b>{userLegajo}</b></li>
+      {/* Contenido principal, siempre cargándose */}
+      <div className={`bar ${!introFinished ? 'hidden-content' : ''}`}>
+        {/* Menú principal a la izquierda */}
+        <div className="logoMenú">
+          <nav>
+            <ul className="menuHorizontal">
               <li>
-                <button className="logoutButton" onClick={() => {
-                  localStorage.clear();
-                  window.location.reload();
-                }}>
-                  Cerrar sesión
-                </button>
+                <Link>
+                  <img className="Logo" src={Logo} alt="logo de la pagina" />
+                </Link>
+                <ul className="menuVertical">
+                  <li>
+                    <Link to="/">
+                      <button><b>Home</b></button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/empleados">
+                      <button><b>Empleados</b></button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/carreras">
+                      <button><b>Carreras</b></button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/piezas">
+                      <button><b>Piezas</b></button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/pistas">
+                      <button><b>Pistas</b></button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/Estrategia">
+                      <button><b>Estrategia</b></button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/Departamentos">
+                      <button><b>Departamento</b></button>
+                    </Link>
+                  </li>
+                </ul>
               </li>
             </ul>
-          </>
-        ) : (
-          <Link to="/login">
-            <button className="loginButton">Iniciar sesión</button>
-          </Link>
-        )}
+          </nav>
+        </div>
+
+        {/* Logo central */}
+        <Link to="/">
+          <img className="Logo_central" src={Logocentral} alt="Logo de en medio" />
+        </Link>
+
+        {/* Menú de usuario a la derecha */}
+        <div className="userMenu">
+          {userName ? (
+            <>
+              <div className="userInfo">
+                <p className="user">{`${userName} - ${userDepartment}`}</p>
+              </div>
+              <ul className="menuVerticalUser">
+                <li><b>{userName}</b></li>
+                <li><b>{userDepartment}</b></li>
+                <li><b>{userLegajo}</b></li>
+                <li>
+                  <button className="logoutButton" onClick={() => {
+                    localStorage.clear();
+                    window.location.reload();
+                  }}>
+                    Cerrar sesión
+                  </button>
+                </li>
+              </ul>
+            </>
+          ) : (
+            <Link to="/login">
+              <button className="loginButton">Iniciar sesión</button>
+            </Link>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
