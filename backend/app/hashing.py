47f62420b9d42a0f76c55751bf77a5520df67dba
Conraaa <c.cemino1234@gmail.com>
2025-02-18 18:27:19 -0300

    aa

--- backend/app/hashing.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.hashers import check_password
-from .models import Usuario  # Adjust the import path as necessary
-
-usuario = Usuario.objects.get(legajo=1548554)
-hash_bd = usuario.contrasenia
-
-print(f"La contraseña es correcta? {check_password('default', hash_bd)}")  
