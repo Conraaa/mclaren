2ec0a4699a81f7e31b8d82e2949c71ce5fb88025
Conraaa <c.cemino1234@gmail.com>
2025-02-22 18:50:49 -0300

    variables de entorno y host deploy

--- backend/mclaren/settings.py
+++ backend/mclaren/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mclaren-production.up.railway.app"]
 
 
 # Application definition
