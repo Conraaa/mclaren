93819a542cde4d081ca5bc5f2749f0be5d81b512
Mauro Jose Lista Marin <126710594+Mauricitoxx@users.noreply.github.com>
2025-02-23 03:25:37 -0300

    Update urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,6 +3,7 @@ from . import views
 from rest_framework.routers import DefaultRouter
 from .views import TelemetriasDeCarrera
 from .views import RegistrosDeTelemetria
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 router = DefaultRouter()
 router.register('departamentos', views.DepartamentoViewSet)
 router.register('usuarios', views.UsuarioViewSet)
@@ -16,6 +17,8 @@ router.register('telemetrias', views.TelemetriaViewSet)
 router.register('registros', views.RegistroViewSet)
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/registros/<int:telemetria_id>/', RegistrosDeTelemetria.as_view(), name='registros_de_telemetria'),
     path('api/telemetrias/<int:carrera_id>/', TelemetriasDeCarrera.as_view(), name='telemetrias_de_carrera'), path('api/login/', views.LoginView.as_view(), name='login'),
     path('verificar-legajo/', views.verificar_legajo, name='verificar_legajo'),
