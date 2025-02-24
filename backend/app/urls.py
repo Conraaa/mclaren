f8ebec9b8cfa60fcd804aab5f06a98188ac9ebf6
Mauro Jose Lista Marin <126710594+Mauricitoxx@users.noreply.github.com>
2025-02-24 19:12:14 -0300

    Update urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-<<<<<<< HEAD
 from . import views
 from .views import TelemetriasDeCarrera, RegistrosDeTelemetria
-
-=======
-from .views import TelemetriasDeCarrera
-from .views import RegistrosDeTelemetria
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
->>>>>>> 63604334241dcba32d1c0bd92bcb00bab6274792
 router = DefaultRouter()
 router.register('departamentos', views.DepartamentoViewSet)
 router.register('usuarios', views.UsuarioViewSet)
@@ -22,13 +16,11 @@ router.register('telemetrias', views.TelemetriaViewSet)
 router.register('registros', views.RegistroViewSet)
 
 urlpatterns = [
-<<<<<<< HEAD
 
 
-=======
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
->>>>>>> 63604334241dcba32d1c0bd92bcb00bab6274792
     path('api/registros/<int:telemetria_id>/', RegistrosDeTelemetria.as_view(), name='registros_de_telemetria'),
     path('api/telemetrias/<int:carrera_id>/', TelemetriasDeCarrera.as_view(), name='telemetrias_de_carrera'),
     path('api/login/', views.LoginView.as_view(), name='login'),
