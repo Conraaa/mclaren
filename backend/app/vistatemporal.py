18c9de0bcddec3cd9f31763cda00362b3d3d5a14
Conraaa <c.cemino1234@gmail.com>
2025-03-01 03:42:51 -0300

    eliminar pistas

--- backend/app/vistatemporal.py
+++ backend/app/vistatemporal.py
@@ -8,21 +8,7 @@ import cloudinary.uploader
 def eliminar_pista(request, id):
     try:
         pista = Pista.objects.get(id=id)
-
-        # Verifica el contenido de pista.imagen
-        print(f"Contenido de pista.imagen: {pista.imagen}")
-
-        if pista.imagen:
-            print(f"Tiene imagen")
-            # Obtener el public_id a partir del campo imagen
-            public_id = pista.imagen.public_id if pista.imagen.public_id else pista.imagen.name.split("/")[-1].split(".")[0]
-            
-            # Elimina la imagen de Cloudinary
-            print(f"Eliminando imagen con public_id: {public_id}")
-            cloudinary.uploader.destroy(public_id)
-
-        # Elimina la pista de la base de datos
-        print(f"Linea antes de eliminar pista")
+        print(f"Linea antes de eliminar, pista:", pista)
         pista.delete()
         print(f"Pista eliminada de la base de datos")
         return Response({"message": "Pista eliminada correctamente"}, status=200)
@@ -31,6 +17,7 @@ def eliminar_pista(request, id):
         return Response({"error": "Pista no encontrada"}, status=404)
     except Exception as e:
         return Response({"error": str(e)}, status=500)
+
 
 
 @api_view(['GET'])
