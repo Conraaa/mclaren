b854683df05d3ae2c49c73af5b97aac346bd3c9d
Conraaa <c.cemino1234@gmail.com>
2025-03-01 03:32:57 -0300

    eliminar pistas

--- backend/app/vistatemporal.py
+++ backend/app/vistatemporal.py
@@ -2,11 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from app.models import Pista
 import cloudinary
-
 import cloudinary.uploader
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from app.models import Pista
 
 @api_view(['DELETE'])
 def eliminar_pista(request, id):
@@ -17,25 +13,24 @@ def eliminar_pista(request, id):
         print(f"Contenido de pista.imagen: {pista.imagen}")
 
         if pista.imagen:
-            print(f"Tiene imegen")
-            if hasattr(pista.imagen, 'public_id'):
-                public_id = pista.imagen.public_id
-            else:
-                # Si no tiene public_id, intenta obtenerlo de la URL
-                public_id = pista.imagen.name.split("/")[-1].split(".")[0]
-                
+            print(f"Tiene imagen")
+            # Obtener el public_id a partir del campo imagen
+            public_id = pista.imagen.public_id if pista.imagen.public_id else pista.imagen.name.split("/")[-1].split(".")[0]
+            
             # Elimina la imagen de Cloudinary
+            print(f"Eliminando imagen con public_id: {public_id}")
             cloudinary.uploader.destroy(public_id)
 
         # Elimina la pista de la base de datos
+        print(f"Linea antes de eliminar pista")
         pista.delete()
-        print(f"Paso el delete")
+        print(f"Pista eliminada de la base de datos")
         return Response({"message": "Pista eliminada correctamente"}, status=200)
+    
     except Pista.DoesNotExist:
         return Response({"error": "Pista no encontrada"}, status=404)
     except Exception as e:
         return Response({"error": str(e)}, status=500)
-
 
 
 @api_view(['GET'])
